289f7a858e230f6386dc058126e9dca0fcdba361
mosesbalaji <moses.balaji-external@tcs.com>
2022-01-07 18:07:24 +0800

    test

---
+++ index.js
@@ -0,0 +1,51 @@
+const fs = require('fs')
+  , path = require('path')
+  , core = require('@actions/core')
+  , io = require('@actions/io')
+  , json2csv = require('json2csv')
+  , github = require('@actions/github')
+  , githubClient = require('./src/githublib/githubClient')
+  , OrganizationActivity = require('./src/OrgsUserActivity')
+  , Organization = require('./src/githublib/Organization')
+;
+
+async function run() {
+  const token = core.getInput('token')
+    , outputDir = core.getInput('outputDir')
+    , organizationinp = core.getInput('organization')
+    , maxRetries = core.getInput('octokit_max_retries')
+  ;
+console.log(organizationinp)
+let regex = /^[\w\.\_\-]+((,|-)[\w\.\_\-]+)*[\w\.\_\-]+$/g;
+let validate_org = regex.test(organizationinp);
+if((!validate_org)) {
+  throw new Error('Provide a valid organization - It accept only comma separated value');
+}
+
+let sinceregex = /^(20)\d\d-(0[1-9]|1[012])-([012]\d|3[01])T([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/ 
+;
+
+await io.mkdirP(outputDir)
+
+const octokit = githubClient.create(token, maxRetries)
+  , orgActivity = new OrganizationActivity(octokit)
+  , orgActivity1 = new Organization(octokit)
+;
+
+//***start */
+let organizationlist = organizationinp.split(',');
+let removeMulUserList = [];
+let repos = [];
+let rmvconfrm = 0;
+console.log(organizationlist)
+for(const organization of organizationlist){
+  console.log(`Attempting to generate ${organization} - user activity data, this could take some time...`);
+  const orgsComments = await orgActivity.getOrgsValid(organization);
+  if(orgsComments.status !== 'error') {
+       repos = await orgActivity1.getRepositories(organization);
+  }
+}
+core.setOutput('repos', repos);
+}
+
+run();
